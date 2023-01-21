3546915d2c96b3f4513f1aa28e9cea3130976c56
Jocelyn Snodgrass <jgayle@email.unc.edu>
2023-01-21 15:14:39 -0500

    final ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,41 +2,35 @@
 __author__ = "730412085"
 
 Ch_word: str = input("Enter a 5-character word: ")
-if(len(Ch_word) != 5):
+if (len(Ch_word) != 5):
     print("Error: Word must contain 5 characters")
     exit()
-
 Ch_letter: str = input("Enter a single character: ")
-Ch_count: int = 0
-
+if (len(Ch_letter) != 1):
+    print("Error: Character must be a single character.")
+    exit()
 print("Searching for " + Ch_letter + " in " + Ch_word)
 
-if(Ch_letter == Ch_word[0]):
+Ch_count: int = 0
+if (Ch_letter == Ch_word[0]):
     print(Ch_letter + " found at index 0")
     Ch_count = Ch_count + 1
-
-if(Ch_letter == Ch_word[1]):
+if (Ch_letter == Ch_word[1]):
     print(Ch_letter + " found at index 1")
     Ch_count = Ch_count + 1
-
-if(Ch_letter == Ch_word[2]):
+if (Ch_letter == Ch_word[2]):
     print(Ch_letter + " found at index 2")
     Ch_count = Ch_count + 1
-
-if(Ch_letter == Ch_word[3]):
+if (Ch_letter == Ch_word[3]):
     print(Ch_letter + " found at index 3")
     Ch_count = Ch_count + 1
-
-if(Ch_letter == Ch_word[4]):
+if (Ch_letter == Ch_word[4]):
     print(Ch_letter + " found at index 4")
     Ch_count = Ch_count + 1
 
-
-if(Ch_count == 0):
+if (Ch_count == 0):
     print("No instances of " + Ch_letter + " found in " + Ch_word)
-if(Ch_count == 1):
+if (Ch_count == 1):
     print("1 instance of " + Ch_letter + " found in " + Ch_word)
-if(Ch_count > 1):
-    Ch_count =str(Ch_count)
-    print(Ch_count + " instances of " + Ch_letter + " found in " + Ch_word)
-
+if (Ch_count > 1):
+    print(str(Ch_count) + " instances of " + Ch_letter + " found in " + Ch_word)
