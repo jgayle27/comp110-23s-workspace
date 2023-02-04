7156373c11d78a04f919aa72a804bf57594e2dc8
Jocelyn Snodgrass <jgayle@email.unc.edu>
2023-02-04 14:40:58 -0500

    Final ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,52 +2,58 @@
 __author__ = "730412085"
 
 secret: str = "python"
-lensecret: int = len(secret)
-guess: str = input(f"What is your {lensecret}-letter guess? ")
+len_secret: int = len(secret)
+guess: str = input(f"What is your {len_secret}-letter guess? ")
 index: int = 0
 numb_guess: int = 0
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 result = str()
+"""Whether a letter in a guess should have a yellow box"""
 yellow_check: int = 0
+"""What the yellow/white box loop runs on"""
 color_bool: bool = False
 
-"""Make sure guess is x letters, with each mistake guess count goes up"""
-while(len(guess) != lensecret):
+"""To make sure guess is x letters, with each mistake guess count goes up until a maximum of 4 guesses"""
+while(len(guess) != len_secret):
     if(numb_guess < 4):
-        guess = input(f"That was not {lensecret} letters! Try again: ")
+        guess = input(f"That was not {len_secret} letters! Try again: ")
         numb_guess = numb_guess + 1
     else:
         print("Not quite. Play again soon!")
         exit()
 
-"""guess is x letters, so output boxes"""
-while(index < lensecret):
-    color_bool = False
+"""Guess is x letters, so output respective colored boxes"""
+while(index < len_secret):
     yellow_check = 0
+    color_bool = False
     if(guess[index] == secret[index]):
         result = (f"{result}{GREEN_BOX}")
         index = index+1
     else:
-        while(color_bool != True):
-            if(guess[index] == secret[yellow_check]):
-                color_bool == True
-            else:
-                yellow_check = yellow_check + 1
-        if(color_bool == True):
-            result = (f"{result}{YELLOW_BOX}")
-        else: 
+        while(color_bool == False):
+            while(yellow_check < len_secret):
+                if(guess[index] == secret[yellow_check]):
+                    color_bool = True
+                    yellow_check = len_secret
+                    result = (f"{result}{YELLOW_BOX}")
+                    index = index + 1
+                else:
+                    yellow_check = yellow_check + 1
+                    if(yellow_check == len_secret):
+                        color_bool = True
             result = (f"{result}{WHITE_BOX}")      
-        index = index + 1
-        
+            index = index + 1
 
-"""Results: if guess is correct green boxes, if incorrect green/white boxes"""
+"""Reset variables"""
+yellow_check = 0
+color_bool = False
+
+"""Results: if guess is correct all green boxes, if incorrect green/yellow/white boxes with corresponding notes"""
 if(guess == secret):
         print(f"{result} \nWoo you got it!")
         exit()
 else:
         print(f"{result} \nNot quite. Play again soon!")
         exit()
-
-#python -m exercises.ex02_one_shot_wordle
