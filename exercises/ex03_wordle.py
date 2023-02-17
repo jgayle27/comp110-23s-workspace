a4079a93c5afc1766f5e3609ef691d06f8253a06
Jocelyn Snodgrass <jgayle@email.unc.edu>
2023-02-17 16:20:45 -0500

    second update to exercise 3

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,42 +1,27 @@
 """EX03 - Structured Wordle"""
 __author__ = "730412085"
 
-secret: str = "codes"
-len_secret: int = len(secret)
-guess_input: str = input(f"What is your {len_secret}-letter guess? ")
-"""indexes guess_char and secret in function"""
-guess_char_count = 0
-guess_char_index = 0
-guess_char: str = guess_input[guess_char_index]
-"""Whether a letter in a guess should have a yellow box"""
-yellow_check: int = 0
-"""What the yellow/white box loop runs on"""
-color_bool: bool = False
 
+"""CODE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"""
 def contains_char(secret: str, guess_char: str) -> bool:
     """Compares the secret's letters to the guess's letters and outputs a boolean value"""
     assert len(guess_char) == 1
+    secret_char_count = 0
 
     """Assess letters"""
-    while (guess_char_count < len_secret): 
-        yellow_check = 0
-        color_bool = False
-        if (guess_char == secret[guess_char_count]): 
-            guess_char_count = guess_char_count + 1
-            guess_char_index = guess_char_index + 1
+    while (secret_char_count < len(secret)): 
+        if (guess_char == secret[secret_char_count]): 
             return(True)
         else:
-            while (color_bool is False):            
-                if (guess_char == secret[yellow_check]): 
-                    guess_char_index = guess_char_index + 1
-                    color_bool = True
+            if (guess_char != secret[secret_char_count]): 
+                secret_char_count = secret_char_count + 1
+                if (secret_char_count >= len(secret)): 
                     return(False)
-                else:
-                    if (guess_char != secret[yellow_check]): 
-                        yellow_check = yellow_check + 1
-                        if (yellow_check >= len_secret): 
-                            guess_char_index = guess_char_index + 1
-                            color_bool = True
-                            return(False)
-    guess_char_count = 0
-print(contains_char)
+                        
+
+def emojified():
+    """Output colored boxes according to correctness and secret length"""
+    assert len(guess) == len(secret)
+
+    """Assess boxes needed"""
+    
