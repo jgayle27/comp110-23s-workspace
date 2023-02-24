4509cc6faf007a115f60fc9abf22e2caa4e30c58
Jocelyn Snodgrass <jgayle@email.unc.edu>
2023-02-23 23:30:27 -0500

    Updates to ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,39 +1,45 @@
-"""Developing Utility Functions using lists"""
+"""Developing Utility Functions using lists."""
 __author__ = "7304121085"
 
-from random import randint
 
 utils_list: list[str] = list()
-int = randint(1,3)
+utils_idx = 0
 
-def all(utils_list: list[int],int) -> bool:
-    while(utils_list[i] == int):
-        i = i + 1
-        if(i > len(utils_list)):
-            return(True)
-    return(False)
 
-def max(utils_list: list[int]) -> int:
-    max_int = utils_list[0]
-    idx = 0
+def all(utils_list: list[int], int) -> bool:
+    """Takes a list and compares each item in list to some singular integer."""
+    while (utils_list[utils_idx] == int):
+        utils_idx = utils_idx + 1
+        if(utils_idx > len(utils_list)):
+            return (True)
+    return (False)
 
-    if len(utils_list) == 0:
+
+def max(max_list: list[int]) -> int:
+    """Finds maximum number within a list."""
+    max_int = max_list[0]
+    max_idx = 0
+
+    if len(max_list) == 0:
         raise ValueError("max() arg is an empty List")
-    while(idx <= len(utils_list) - 1):
-        if utils_list[idx] > max_int:
-            max_int = utils_list[idx]
-            idx = idx + 1
+    while (max_idx <= len(max_list) - 1):
+        if max_list[max_idx] > max_int:
+            max_int = max_list[max_idx]
+            max_idx = max_idx + 1
         else:
-            idx = idx + 1
-    return(max_int)
+            max_idx = max_idx + 1
+    return (max_int)
 
 
 def is_equal(first_equal: list[int], second_equal: list[int]) -> bool:
+    """Compares two lists and outputs a boolean depending on whether they are identical or not."""
     e_idx = 0
-    if(len(first_equal) == len(second_equal)):
-        while(first_equal[e_idx] == second_equal[e_idx] and e_idx <= len(first_equal) - 1):
-            e_idx = e_idx + 1
-        return(True)
+    if (len(first_equal) == len(second_equal)):
+        while (e_idx <= len(first_equal) - 1):
+            if (first_equal[e_idx] == second_equal[e_idx]):
+                e_idx = e_idx + 1
+            else:
+                return(False)
+        return (True)
     else:
-        return(False)
-
+        return (False)
