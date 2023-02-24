850d0bdddffaf728a666d68cef72521fb2252167
Jocelyn Snodgrass <jgayle@email.unc.edu>
2023-02-23 23:42:58 -0500

    Final ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,9 +5,11 @@ __author__ = "7304121085"
 def all(utils_list: list[int], input_int: int) -> bool:
     """Takes a list and compares each item in list to some singular integer."""
     utils_idx = 0
-
+    
+    if (len(utils_list) == 0):
+        return (False)
     while (utils_list[utils_idx] == input_int):
-        utils_idx = utils_idx + 1
+        utils_idx += 1
         if (utils_idx > len(utils_list) - 1):
             return (True)
     return (False)
@@ -23,9 +25,9 @@ def max(max_list: list[int]) -> int:
     while (max_idx <= len(max_list) - 1):
         if (max_list[max_idx] > max_int):
             max_int = max_list[max_idx]
-            max_idx = max_idx + 1
+            max_idx += 1
         else:
-            max_idx = max_idx + 1
+            max_idx += 1
     return (max_int)
 
 
@@ -35,7 +37,7 @@ def is_equal(first_equal: list[int], second_equal: list[int]) -> bool:
     if (len(first_equal) == len(second_equal)):
         while (e_idx <= len(first_equal) - 1):
             if (first_equal[e_idx] == second_equal[e_idx]):
-                e_idx = e_idx + 1
+                e_idx += 1
             else:
                 return (False)
         return (True)
