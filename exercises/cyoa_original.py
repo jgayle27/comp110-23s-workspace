c8336d891ed3d3c7fe9ff477f3ba13d11d5d9513
Jocelyn Snodgrass <jgayle@email.unc.edu>
2023-03-21 15:11:50 -0400

    cyoa work

--- exercises/cyoa_original.py
+++ exercises/cyoa_original.py
@@ -4,49 +4,38 @@ __author__ = "730412085"
 
 from random import randint
 
-
-points: int = 0
 player: str
-HORIZONTAL_PLANE: str = "\U0001F6E9"
-SIDEWAYS_PLANE: str = "\U00002708"
-
-
 def main() -> None:
     """Main function containing global variables."""
-    global points
-    points = randint(0,120)
-    print(points)
-    greet()
+    points: int = 0
+    player = greet()
     main_bool = False
     """Present player with at least 3 options of where to go next and ask them to choose."""
     while (main_bool == False):
         option = input(f"How can we help you today, {player}? Please enter the corresponding number from the following options: \n (1) Schedule a flight \n (2) Use my points \n (3) Exit  \n")
         if (option == '1'):
-            flight(player)
-            print(points)
+            flight(player, points)
         if (option == '2'):
             points = rewards(points, player)
             print(points)
         if (option == '3'):
-            print(f"\nSuccessfully logged out. \n \n \n United Airlines\nGood leads the way. {HORIZONTAL_PLANE}")
+            print(f"\nSuccessfully logged out. \n \n \n United Airlines\nGood leads the way.\U0001F6E9")
             main_bool = True
-        
 
 
 
 def greet() -> None:
     """Greets player and them returns to main function."""
-    global player
-    player = input("Welcome to United Airlines Rewards! \n Please enter your name:  ")
-    print(f"Thank you for choosing United, {player}. \nThe United Rewards program is designed to give back to you so you can travel where you want to, anytime. \nWith each flight, you will earn points towards your next adventure. \n    Where will good lead you? \U0001F6E9")
+    customer = input("Welcome to United Airlines Rewards! \n Please enter your name:  ")
+    print(f"Thank you for choosing United, {customer}. \nThe United Rewards program is designed to give back to you so you can travel where you want to, anytime. \nWith each flight, you will earn points towards your next adventure. \n    Where will good lead you? \U0001F6E9")
+    return (customer)
     
 
-def flight (name: str) -> None:
+def flight (name: str, func_points: int) -> None:
     """Gives player choice between rates for flights."""
-    global points
     flight_menu = False
     while (flight_menu == False):
-        print(f"{SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}")
+        print("\U00002708  \U00002708  \U00002708  \U00002708  \U00002708  \U00002708  \U00002708  \U00002708  \U00002708  \U00002708  \U00002708  \U00002708")
         print(f"United Flights \n \nWhere will you go, {name}?")
         player_origin = input("Enter city of origin:  ")
         player_destination = input("Enter destination city:  ")
@@ -54,38 +43,53 @@ def flight (name: str) -> None:
         rates_high = randint(301, 600)
         rates_choice = input(f"{player_origin} to {player_destination} rates calculated from ${rates_low} to ${rates_high}. \nSelect from these rates? Enter 'yes' or 'no'.  ")
         if (rates_choice == "yes"):
-            points = rates_func(player_origin, player_destination, rates_low, rates_high, points)
+            rates_func(player_origin, player_destination, rates_low, rates_high, func_points)
             flight_menu = True
         else:
             return_main = input("Do you wish to return to the main menu or the flight selection menu? Enter 'main menu' or 'flight menu'.  ")
-            if (return_main != 'main menu'):
+            if (return_main == 'main menu'):
+                return ()
+            else:
                 print(f"Please select a new origin and destination.\n")
+    return ()
 
 
-def rates_func(input_a: str, input_b: str, input_c: int, input_d: int, rates_points: int) -> int:
+def rates_func(input_a: str, input_b: str, input_c: int, input_d: int, rates_points: int) -> None:
     """Inputs destination and origin and rate bounds to give player the opportunity to choose a rate and flight number to book a flight."""
+    rate_decision = False
     option_num = 1
-    weekdays: list = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-    weekday_choice = weekdays[randint(0,6)]
-    flight_points = randint(20, 50)
-    print(f"\n------\nFlight option {option_num}: {input_a} to {input_b}\nprice: ${randint(input_c, input_d)}\n")
-    flight_decision = input("Select this flight? Enter 'yes' or 'no'  ")
-    if (flight_decision == 'yes'):
-        print(f"United Rewards points for Flight {option_num}: {flight_points}\nDeparts {weekday_choice}\n")
-        final_decision = input(f"Confirm {flight_decision}? Enter 'yes' or 'no'.  ")
-        if (final_decision == 'yes'):
-            print(f"\nFlight {option_num} from {input_a} to {input_b} confirmed for {weekday_choice}\nUnited rewards points earned: {flight_points}\n \nYou may cancel with no penalty anytime up until 12 hours before the start of your departure day. \nOur Help Desk is happy to assist you with any questions you may have.\n\n{SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}  {SIDEWAYS_PLANE}")
-            rates_points = rates_points + flight_points
-        else:
-            return_decision = input("Do you wish to return to the main menu or to view an alternate flight for your trip? Enter 'main menu' or 'view new flight'.  ")
-            if (return_decision != 'main menu'):
-                option_num += 1
-    return(rates_points)
+    weekdays: list = ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
+    while (rate_decision == False):
+        weekday_choice = weekdays[randint(0,6)]
+        flight_points = randint(20, 50)
+        print(f"\n------\nFlight option {option_num}: {input_a} to {input_b}\nprice: ${randint(input_c, input_d)}\n")
+        flight_decision = input("Select this flight? Enter 'yes' or 'no'  ")
+        if (flight_decision == 'yes'):
+            print(f"United Rewards points for Flight {option_num}: {flight_points}\nDeparts {weekday_choice}\n")
+            final_decision = input(f"Confirm {flight_decision}? Enter 'yes' or 'no'.  ")
+            if (final_decision == 'yes'):
+                print(f"\nFlight {option_num} from {input_a} to {input_b} confirmed for {weekday_choice}\nUnited rewards points earned: {flight_points}\n \nYou may cancel with no penalty anytime up until 12 hours before the start of your departure day. \nAll Rewards points earned will be added to your account within 4 hours. \nOur Help Desk is happy to assist you with any questions you may have.\n\n\U0001F6EB  \U0001F6EB  \U0001F6EB")
+                rates_points = rates_points + flight_points
+                rate_decision = True
+            else:
+                return_decision = input("Do you wish to return to the main menu or to view an alternate flight for your trip? Enter 'main menu' or 'view new flight'.  ")
+                if (return_decision == 'main menu'):
+                    rate_decision = True
+                else:
+                    option_num += 1
 
+        else:
+            return_decision2 = input("Do you wish to return to the main menu or to view an alternate flight for your trip? Enter 'main menu' or 'view new flight'.  ")
+            if (return_decision2 == 'main menu'):
+                return ()
+            else:
+                option_num += 1
+    return ()
 
             
 def rewards(rewards_points: int, rewards_name: str) -> int:
     """Inputs points and player name to give player the opportunity to choose what they want to do with their points."""
+    rewards_points += randint(0,120)
     print(f"Thank you for flying with us, {rewards_name}!\nYour current Rewards points total is {rewards_points}")
     rewards_choice = input("Would you like to use your points or return to the main menu? Enter 'use points' or 'main menu'.  ")
     if (rewards_choice == 'use points'):
@@ -99,7 +103,8 @@ def rewards(rewards_points: int, rewards_name: str) -> int:
             rewards_points = discount_flight(rewards_points)
         if (points_use == '3'):
             rewards_points = wifi(rewards_points)
-    return(rewards_points)
+    else:
+        return(rewards_points)
 
 def seat_upgrade(seat_points: int) -> int:
     """Inputs points and flight number to determine seat upgrade cost and outputs new point amount."""
@@ -198,10 +203,12 @@ def wifi(wifi_points: int) -> int:
                 return (wifi_points)
             else:
                 wifi_points = wifi_points - 8
-                print(f"Your current Rewards points balance is {wifi_points}. Thank you for choosing United. We look forward to seeing you{SIDEWAYS_PLANE}\n----\nRedirecting you to the main menu.\n")
+                print(f"Your current Rewards points balance is {wifi_points}. Thank you for choosing United. We look forward to seeing you\U00002708\n----\nRedirecting you to the main menu.\n")
                 return (wifi_points)
     else:        
         return (wifi_points)
+
+
 
 
 if __name__ == "__main__":
