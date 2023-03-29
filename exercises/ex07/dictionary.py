c7412e3b3da8e8652cdcd2c3cffa5b9ffc33ecfc
Jocelyn Snodgrass <jgayle@email.unc.edu>
2023-03-29 00:05:29 -0400

    ex07 final rough draft

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -4,7 +4,8 @@
 __author__ = "730412085"
 
 
-def invert (i_dict: dict[str, str]) -> dict[str, str]:
+def invert(i_dict: dict[str, str]) -> dict[str, str]:
+    """Takes a dictionary of strings and switches the keys and values and then outputs a dictionary of these switched keys as values and values as keys."""
     invert_list: list[str] = []
     new_dict: dict[str, str] = {}
     idx: int = 0
@@ -13,16 +14,17 @@ def invert (i_dict: dict[str, str]) -> dict[str, str]:
         for key in i_dict:
             while check < len(invert_list):
                 if i_dict[key] == invert_list[check]:
-                    raise KeyError("cannot have two identical keys!")
+                    raise KeyError("cannot have identical keys!")
                 check += 1
             check = 0
             invert_list.append(i_dict[key])
             new_dict[invert_list[idx]] = key
             idx += 1
-    return(new_dict)
+    return (new_dict)
         
 
-def favorite_color (color_dict: dict[str, str]) -> str:
+def favorite_color(color_dict: dict[str, str]) -> str:
+    """Takes a dictionary of keys and corresponding color values and then tallies which colors recieve the most number of "votes" and then outputs a single str of the top-rated color (if colors are "voted" equally it outputs the first color of the dictionary from that list of equivalent appearances of colors)."""
     color_count: int = 0
     new_dict: dict[str, int] = {}
     color_list: list[str] = []
@@ -30,7 +32,7 @@ def favorite_color (color_dict: dict[str, str]) -> str:
     top_color_list: list[str] = []
     idx: int = 0
     top_color: str = ()
-    #test_idx: int = 0
+    # test_idx: int = 0
     for key in color_dict:
         new_dict[color_dict[key]] = 0
         color_list.append(color_dict[key])
@@ -40,7 +42,7 @@ def favorite_color (color_dict: dict[str, str]) -> str:
         color_count += 1
     for key in new_dict:
         compare_list.append(new_dict[key])
-    #for key in new_dict:
+    # for key in new_dict:
     #    while (idx <= len(new_dict) - 1):
     #        if (new_dict[key] > compare_list[idx]):
     #            top_color = key
@@ -62,17 +64,15 @@ def favorite_color (color_dict: dict[str, str]) -> str:
                 idx += 1
         idx = 0
         top_color = top_color_list[0]
-    return(top_color)
+    return (top_color)
 
 
-def count (count_list: list[str]) -> dict[str, int]:
+def count(count_list: list[str]) -> dict[str, int]:
+    """Takes a list of strings and counts how many times that string is found. Count then takes those strings and the number of appearances and puts them into a dictionary where the strings are the keys and the number of appearances are the int values."""
     new_dict: dict[str, int] = {}
     for elem in count_list:
         if elem in new_dict:
             new_dict[elem] += 1
         if elem not in new_dict:
             new_dict[elem] = 1
-    return(new_dict)
-
-count_practice: list = ("cat","dog","fish","cat","fish")
-print(count(count_practice))
+    return (new_dict)
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -7,14 +7,14 @@ from exercises.ex07.dictionary import invert, favorite_color, count
 
 def test_normal_use_invert() -> None:
     """Test using invert and a dictionary of several strs, results in dictionary with values as the keys and the keys as values."""
-    test_dictionary = {"a":"!", "b":"*", "c":"3", "d":"()"}
-    assert invert(test_dictionary) == {"!":"a", "*":"b", "3":"c", "()":"d"}
+    test_dictionary = {"a": "!", "b": "*", "c": "3", "d": "()"}
+    assert invert(test_dictionary) == {"!": "a", "*": "b", "3": "c", "()": "d"}
 
 
 def test_normal_use_with_small_dictionary() -> None:
     """Test using invert and a dictionary where the values are duplicates so the result would have duplicate key values, results in KeyError."""
-    test_dictionary = {"cat":"fish"}
-    assert invert(test_dictionary) == {"fish":"cat"}
+    test_dictionary = {"cat": "fish"}
+    assert invert(test_dictionary) == {"fish": "cat"}
 
 
 def test_empty_dict() -> None:
@@ -23,7 +23,7 @@ def test_empty_dict() -> None:
     assert invert(test_dictionary) == {}
 
 
-#def test_normal_use_with_identical_keys() -> None:
+# def test_normal_use_with_identical_keys() -> None:
 #    """Test using invert and a dictionary where the values are duplicates so the result would have duplicate key values, results in KeyError."""
 #    test_dictionary = {"a":"!", "b":"*", "c":"3", "d":"!"}
 #    assert invert(test_dictionary) == KeyError("cannot have two identical keys!")
@@ -31,13 +31,13 @@ def test_empty_dict() -> None:
 
 def test_normal_use_color() -> None:
     """Tests favorite_color using a normal sized dictionary with a duplicate of one value, results in a str of that value."""
-    test_color_dict = {"Felicia":"green", "Skippy":"purple", "Toulouse":"blue", "Max":"blue"}
+    test_color_dict = {"Felicia": "green", "Skippy": "purple", "Toulouse": "blue", "Max": "blue"}
     assert favorite_color(test_color_dict) == "blue"
 
 
 def test_normal_use_with_unique_values() -> None:
     """Tests favorite_color using a normal sized dictionary with an equal amount of values, results in the first value as a str."""
-    test_color_dict = {"Felicia":"green", "Skippy":"purple", "Toulouse":"blue", "Max":"red"}
+    test_color_dict = {"Felicia": "green", "Skippy": "purple", "Toulouse": "blue", "Max": "red"}
     assert favorite_color(test_color_dict) == "green"
 
 
@@ -49,14 +49,14 @@ def test_empty_dictionary() -> None:
 
 def test_normal_use_count() -> None:
     """Tests count using a normal sized list and duplicates of various values, results in a dictionary with the value as a str and the number of occurences as an int."""
-    test_count_list = ("cat","dog","fish","cat","fish")
-    assert count(test_count_list) == {"cat":2, "dog":1, "fish":2}
+    test_count_list = ("cat", "dog", "fish", "cat", "fish")
+    assert count(test_count_list) == {"cat": 2, "dog": 1, "fish": 2}
 
 
 def test_normal_use_with_unique_list() -> None:
     """Tests count using a list with completely unique variables, results in a dictionary where every key has the value of 1."""
     test_count_list = ("cat", "dog", "fish")
-    assert count(test_count_list) == {"cat":1, "dog":1, "fish":1}
+    assert count(test_count_list) == {"cat": 1, "dog": 1, "fish": 1}
 
 
 def test_count_with_empty_list() -> None:
