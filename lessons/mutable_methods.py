2f15e0b15917c75d4d2a34f16cfe08beca5acf16
Jocelyn Snodgrass <jgayle@email.unc.edu>
2023-04-11 10:05:43 -0400

    mutable_methods lesson changes

--- lessons/mutable_methods.py
+++ lessons/mutable_methods.py
@@ -26,9 +26,26 @@ class Point:
         """Print prettier version of out point."""
         return f"({self.x},{self.y})"
     
+    def __mul__(self, factor: float) -> float:
+        scaled: Point = Point(self.x * factor, self.y*factor)
+        return(scaled)
+    
+    def __getitem__(self, index: int) -> float:
+        """Overloading subscription notation."""
+        if index == 0:
+            return self.x
+        else:
+            if index == 1:
+                return self.y
+            else: 
+                raise IndexError
+    
 a: Point = Point(1.0, 2.0)
-b: Point = a.scale(3.0)
-print(str(a))
-print(a)
+#b: Point = a.scale(3.0)
+b: Point = a * 3.0
+# print(str(a))
+# print(a)
+print(b)
 print(f"My point is {b}")
+print(b[1]) # gives y-coordinate, b[0] is x-coordinate
 
